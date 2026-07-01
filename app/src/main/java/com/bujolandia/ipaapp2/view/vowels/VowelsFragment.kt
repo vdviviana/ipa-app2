b8898a4ffce4e0255cd4958c689ced24badedb18
olga <o.lopez@gl-tours.com>
2026-07-01 02:36:12 -0300

    todas las pantallas con sus botones co nletras. La navegacion funciona completa

--- app/src/main/java/com/bujolandia/ipaapp2/view/vowels/VowelsFragment.kt
+++ app/src/main/java/com/bujolandia/ipaapp2/view/vowels/VowelsFragment.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+
 import com.bujolandia.ipaapp2.R
+import com.bujolandia.ipaapp2.view.home.HomeFragment
 
 class VowelsFragment : Fragment() {
 
@@ -15,5 +18,22 @@ class VowelsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_vowels, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val btnVolver = view.findViewById<Button>(R.id.btnVolver)
+
+        btnVolver.setOnClickListener {
+            openFragment(HomeFragment())
+        }
+    }
+
+    private fun openFragment(fragment: Fragment) {
+        parentFragmentManager.beginTransaction()
+            .replace(R.id.nav_host_fragment, fragment)
+            .addToBackStack(null)
+            .commit()
     }
 }

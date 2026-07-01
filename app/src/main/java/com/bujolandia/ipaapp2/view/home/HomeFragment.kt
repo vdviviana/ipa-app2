e5594f261fa9c58571d89b3b5875ffa5ea44d0b7
olga <o.lopez@gl-tours.com>
2026-06-30 21:08:36 -0300

    Crea navegación manual entre pantallas principales

---
+++ app/src/main/java/com/bujolandia/ipaapp2/view/home/HomeFragment.kt
@@ -0,0 +1,51 @@
+package com.bujolandia.ipaapp2.view.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+
+import com.bujolandia.ipaapp2.R
+import com.bujolandia.ipaapp2.view.consonants.ConsonantsFragment
+import com.bujolandia.ipaapp2.view.diphthongs.DiphthongsFragment
+import com.bujolandia.ipaapp2.view.vowels.VowelsFragment
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val btnVowels = view.findViewById<Button>(R.id.btnVowels)
+        val btnConsonants = view.findViewById<Button>(R.id.btnConsonants)
+        val btnDiphthongs = view.findViewById<Button>(R.id.btnDiphthongs)
+
+        btnVowels.setOnClickListener {
+            openFragment(VowelsFragment())
+        }
+
+        btnConsonants.setOnClickListener {
+            openFragment(ConsonantsFragment())
+        }
+
+        btnDiphthongs.setOnClickListener {
+            openFragment(DiphthongsFragment())
+        }
+    }
+
+    private fun openFragment(fragment: Fragment) {
+        parentFragmentManager.beginTransaction()
+            .replace(R.id.nav_host_fragment, fragment)
+            .addToBackStack(null)
+            .commit()
+    }
+}
